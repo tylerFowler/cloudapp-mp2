4b5869f0d3439e811c709cea2f74c9c6d457371e
Tyler Fowler <tylerfowler.1337@gmail.com>
2015-09-05 20:33:44 -0500

    Added extra output

--- build.sh
+++ build.sh
@@ -5,12 +5,16 @@ read assignment_name
 
 echo "Building $assignment_name"
 
+echo "Cleaning builds..."
 rm -rf ./build/* ./${assignment_name}.jar
 
+
+echo "Starting build"
 export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar
 hadoop com.sun.tools.javac.Main ${assignment_name}.java -d build
 jar -cvf ${assignment_name}.jar -C build/ ./
 
+echo "Build comleted, running $assignment_name"
 case assignment_name in
   TitleCount)
     hadoop jar TitleCount.jar TitleCount -D stopwords=/mp2/misc/stopwords.txt -D delimeters=/mp2/misc/delimiters.txt /mp2/titles /mp2/A-output
