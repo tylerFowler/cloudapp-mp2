a9cee1d30ff5e1bcf7551d5e57b7ea71918e046a
Tyler Fowler <tylerfowler.1337@gmail.com>
2015-09-05 20:43:35 -0500

    Removed hadoop job running

--- build.sh
+++ build.sh
@@ -14,9 +14,4 @@ export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar
 hadoop com.sun.tools.javac.Main ${assignment_name}.java -d build
 jar -cvf ${assignment_name}.jar -C build/ ./
 
-echo "Build comleted, running $assignment_name"
-case assignment_name in
-  TitleCount)
-    hadoop jar TitleCount.jar TitleCount -D stopwords=/mp2/misc/stopwords.txt -D delimeters=/mp2/misc/delimiters.txt /mp2/titles /mp2/A-output
-    ;;
-esac
+echo "Build completed"
