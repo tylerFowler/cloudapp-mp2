d37b46436f144beaea92bf3d5ba0f112016a31b4
Tyler Fowler <tylerfowler.1337@gmail.com>
2015-09-06 01:04:41 -0500

    Trying this again...

--- OrphanPages.java
+++ OrphanPages.java
@@ -45,7 +45,8 @@ public class OrphanPages extends Configured implements Tool {
     public static class LinkCountMap extends Mapper<Object, Text, IntWritable, IntWritable> {
       @Override
       public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-        Integer pageId = Integer.parseInt(key.toString().replace(':', ' ').trim());
+        String pageIdStr = (String) key;
+        Integer pageId = Integer.parseInt(pageIdStr.replace(':', ' ').trim());
         String[] pageLinks = value.toString().split(" ");
         // ensure that this page gets added because it may not be linked to
         context.write(new IntWritable(pageId), new IntWritable(0));
