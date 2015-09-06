fb5a5380a4915ccf6f59a2f19f209fdfb018ec9e
Tyler Fowler <tylerfowler.1337@gmail.com>
2015-09-05 19:59:26 -0500

    Added TitleCount implementation (1st draft)

--- TitleCount.java
+++ TitleCount.java
@@ -71,7 +71,6 @@ public class TitleCount extends Configured implements Tool {
 
         @Override
         protected void setup(Context context) throws IOException,InterruptedException {
-
             Configuration conf = context.getConfiguration();
 
             String stopWordsPath = conf.get("stopwords");
@@ -84,14 +83,27 @@ public class TitleCount extends Configured implements Tool {
 
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            // TODO
+            String line = value.toString();
+            StringTokenizer st = new StringTokenizer(line, this.delimiters);
+
+            while (st.hasMoreTokens()) {
+              String title = st.nextToken();
+              if (!this.stopWords.contains(title.trim().toLowerCase())) {
+                context.write(new Text(title)), new IntWritable(1);
+              }
+            }
         }
     }
 
     public static class TitleCountReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
         @Override
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            // TODO
+            int sum = 0;
+            for (IntWritable val : values) {
+              sum += val.get();
+            }
+
+            context.write(key, new IntWritable(sum));
         }
     }
 }
